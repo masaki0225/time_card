7ae959522713891855b4b335ff57f0e443f7a030
Ubuntu <ubuntu@ip-172-31-27-71.ap-northeast-1.compute.internal>
2020-11-27 12:33:49 +0000

    proto homepage

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TimeCard
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    
+    config.hosts <<  "cc81f512130d435fb20a33ae3721acc1.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
