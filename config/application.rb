7f829615af95a2615a7538465b7fd511463c18c8
masaki0225 <70649051+masaki0225@users.noreply.github.com>
2021-01-03 13:16:16 +0900

    config

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,9 +12,10 @@ module TimeCard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    
+
     config.hosts <<  "cc81f512130d435fb20a33ae3721acc1.vfs.cloud9.ap-northeast-1.amazonaws.com"
 
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
