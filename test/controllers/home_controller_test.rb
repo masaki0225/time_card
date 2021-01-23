e83c43ce5ac5d2b0c37253afe33a4be08e67638d
masaki0225 <70649051+masaki0225@users.noreply.github.com>
2021-01-23 23:21:52 +0900

    responsive

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  
+
+  test "should get home" do
+    get root_path
+    assert_response :success
+    asssert_select "title", "ProjectToDo"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "ProjectToDo"
+  end
 
 end
--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,5 +1,18 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
-  
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_user_path
+    assert_response :success
+  end
+
+  test "should get show" do
+
+  end
 end
